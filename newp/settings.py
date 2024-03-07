2722d01daa86d424a89dd5c323f9580039005f5c
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-03-07 03:33:27 -0800

    up

--- newp/settings.py
+++ newp/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'newproject',
+    'newp',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
 django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
